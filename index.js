53bc89127f6313e3a70dd0ad9fccae8afca24a96
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-05-24 15:01:57 -0300

    new test

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 class MathTest {
 
-//     static test1(x, y) {
-//     return x + y
-// }
+    static test1(x, y) {
+    return x + y
+}
 
-//     static test2(x, y) {
-//     return x - y
-// }
+    static test2(x, y) {
+    return x - y
+}
 
-//     static test3(x, y) {
-//     return x * y
-// }
+    static test3(x, y) {
+    return x * y
+}
 
-//     static test4(x, y) {
-//     return x / y
-// }
+    static test4(x, y) {
+    return x / y
+}
 }
 
 module.exports = MathTest;
