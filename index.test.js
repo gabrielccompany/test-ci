9c5ff21d311197d9a1afc535bab4f1afb2954099
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-05-24 15:01:31 -0300

    comment

--- index.test.js
+++ index.test.js
@@ -1,17 +1,17 @@
 const Math = require ('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(Math.test1(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(Math.test1(1, 2)).toBe(3);
+// });
 
-test('subtracts 3 - 2 to equal 1', () => {
-    expect(Math.test2(3, 2)).toBe(1);
-});
+// test('subtracts 3 - 2 to equal 1', () => {
+//     expect(Math.test2(3, 2)).toBe(1);
+// });
 
-test('multiplies 3 * 2 to equal 6', () => {
-    expect(Math.test3(3, 2)).toBe(6);
-});
+// test('multiplies 3 * 2 to equal 6', () => {
+//     expect(Math.test3(3, 2)).toBe(6);
+// });
 
-test('divides 9 / 3 to equal 3', () => {
-    expect(Math.test4(9, 3)).toBe(3);
-});
+// test('divides 9 / 3 to equal 3', () => {
+//     expect(Math.test4(9, 3)).toBe(3);
+// });
