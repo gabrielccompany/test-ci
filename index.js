0aeb02d5f367998bac40a699ef5c1b485b6fc8e2
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-05-24 14:35:05 -0300

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+
+class MathTest {
+
+    static test1(x, y) {
+    return x + y
+}
+
+    static test2(x, y) {
+    return x - y
+}
+
+    static test3(x, y) {
+    return x * y
+}
+
+    static test4(x, y) {
+    return x / y
+}}
+
+module.exports = MathTest;
